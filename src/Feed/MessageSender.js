0f98d76878cef372e1e018ea2f3f0bc85e1ec785
Dhiren Rajesh <dhirenrajeshk@gmail.com>
2021-08-29 21:38:04 +0530

    Added Message-Sender Files

--- src/Feed/MessageSender.js
+++ src/Feed/MessageSender.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import "./MessageSender.css"
 import { Avatar, Input } from '@material-ui/core';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 export default function MessageSender() {
     const [input, setInput] = useState(' ');
@@ -35,6 +38,20 @@ export default function MessageSender() {
                     />
                     <button onClick={handleSubmit} type="submit">Hidden Submit</button>
                 </form>
+            </div>
+            <div className="messenger-sender-bottom">
+                <div className="messenger-sender-option">
+                    <VideocamIcon style={{color: "red"}} />
+                    <h3>Live Video</h3>
+                </div>
+                <div className="messenger-sender-option">
+                    <PhotoLibraryIcon style={{color: "green"}} />
+                    <h3>Photo/Video</h3>
+                </div>
+                <div className="messenger-sender-option">
+                    <InsertEmoticonIcon style={{color: "orange"}} />
+                    <h3>Feeling/Activity</h3>
+                </div>
             </div>
         </div>
     )
