0c4ccc2944d9f852140b5a1e4bcba2322f54503e
Dhiren Rajesh <dhirenrajeshk@gmail.com>
2021-08-30 21:50:54 +0530

    Added Post File

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -2,12 +2,20 @@ import React from 'react'
 import "./Feed.css"
 import StoryReel from "./StoryReel"
 import MessageSender from "./MessageSender"
+import Post from "./Post"
 
 export default function Feed() {
     return (
         <div className="feed">
             <StoryReel/>
             <MessageSender/>
+            <Post
+                profilePic="https://pbs.twimg.com/profile_images/1184114794103595010/021TqaVX.jpg"
+                message="One Piece > Naruto > Tokyo Revengers"
+                timestamp="1601493943737"
+                imgName="imgName"
+                username="Dhiren Rajesh"
+            />
             {/* {
                 postsData.map(entry => (
                     <Post
---
+++ src/Feed/Post.js
@@ -0,0 +1,40 @@
+import { Avatar } from '@material-ui/core'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React from 'react'
+import "./Post.css"
+
+export default function Post({profilePic, message, timestamp, imgName, username}) {
+    return (
+        <div className="post">
+            <div className="post-top">
+                <Avatar src={profilePic} className="post-avatar" />
+                <div className="post-top-info">
+                    <h3>{username}</h3>
+                    <p>{new Date(parseInt(timestamp)).toUTCString()}</p>
+                </div>
+                <div className="post-options">
+                    <div className="post-op">
+                        <ThumbUpIcon />
+                        <p>Like</p>
+                    </div>
+                    <div className="post-op">
+                        <ChatBubbleOutlineIcon />
+                        <p>Comment</p>
+                    </div>
+                    <div className="post-op">
+                        <NearMeIcon />
+                        <p>Share</p>
+                    </div>
+                    <div className="post-op">
+                        <AccountCircleIcon />
+                        <ExpandMoreIcon />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
